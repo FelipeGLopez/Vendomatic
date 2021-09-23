0865f26e4536a07b5b7b9b4987a1ab1171d3332c
Felipe <fglf18@gmail.com>
2021-09-23 13:05:09 -0300

    Front prettier

--- front/src/components/Beverage/index.js
+++ front/src/components/Beverage/index.js
@@ -1,17 +1,36 @@
 import React from "react";
+import axios from "axios";
 
-const BeverageList = (items) => {
+const BeverageList = (props) => {
+  const { items, setCoins } = props;
+
+  const buyItem = (id) => {
+    axios
+      .put(`http://localhost:8000/inventory/${id}`)
+      .then((response) => response.headers)
+      .then(
+        (data) => {
+          setCoins(data["x-coins"]);
+        },
+        (error) => console.log(error)
+      );
+  };
+
   return (
     items.length >= 0 && (
       <>
         {items.map((item) => {
           return (
-            <>
-              <div>{item.id}</div>
-              <div>{item.name}</div>
-              <div>{item.value}</div>
-              <div>{item.quantity}</div>
-            </>
+            <div key={item.id}>
+              <div>Beverage: {item.name}</div>
+              <div>$ {item.value}</div>
+              <div>Remaining: {item.quantity}</div>
+              <div>
+                <button onClick={() => buyItem(item.id)}>
+                  Buy {item.name}
+                </button>
+              </div>
+            </div>
           );
         })}
       </>
--- front/src/pages/VendomaticMainPage.js
+++ front/src/pages/VendomaticMainPage.js
@@ -1,23 +1,65 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import BeverageList from "../components/Beverage/BeverageList";
+import BeverageList from "../components/Beverage";
+import axios from "axios";
 
 const MachineStorePage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [beverageList, setBeverageList] = useState([]);
 
+  const [coins, setCoins] = useState(0);
+  const [text, setText] = useState("");
+  const insertCoinHandler = (coin) => {
+    axios
+      .put("http://localhost:8000/", { coin })
+      .then((response) => response.headers)
+      .then(
+        (data) => setCoins(data["x-coins"]),
+        (error) => console.log(error)
+      );
+  };
+  const retrieveCoins = () => {
+    axios
+      .delete("http://localhost:8000/")
+      .then((response) => response.headers)
+      .then(
+        (data) => {
+          setText(`${data["x-coins"]} coins retrieved`);
+          setCoins(0);
+        },
+        (error) => console.log(error)
+      );
+  };
+
   useEffect(() => {
     setIsLoading(true);
 
-    const data = fetch("http://localhost:8000/inventory/")
+    fetch("http://localhost:8000/inventory/")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then(
+        (data) => setBeverageList(data),
+        (error) => console.log(error)
+      );
 
-    setBeverageList(data);
+    console.log("beverageList", beverageList);
     setIsLoading(false);
   }, []);
 
-  return !isLoading && <BeverageList items={beverageList} />;
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <BeverageList items={beverageList} setCoins={setCoins} />
+          <div>Current credit: {coins} quarter coins</div>
+          <div>{text}</div>
+          <button onClick={() => insertCoinHandler(1)}>
+            Insert Quarter Coin
+          </button>
+          <button onClick={() => retrieveCoins()}>Get Coins</button>
+        </>
+      )}
+    </>
+  );
 };
 
 export default MachineStorePage;
