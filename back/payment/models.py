e588d458c93779930debc9c4a8a4d0189b75bea8
Felipe <fglf18@gmail.com>
2021-09-22 21:55:30 -0300

    Add Coin model for payment

--- back/payment/models.py
+++ back/payment/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Coin(models.Model):
+    """
+    Generic model for coins
+    """
+
+    class CoinValues(models.TextChoices):
+        QUARTER = "QUARTER"
+
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    type = models.CharField(
+        max_length=20,
+        choices=CoinValues.choices,
+        default=CoinValues.QUARTER,
+    )
