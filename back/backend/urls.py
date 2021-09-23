4b1e00633434081b7d1349ca78d839b5c01c1a4d
Felipe <fglf18@gmail.com>
2021-09-22 22:18:01 -0300

    Updates backend final EP

--- back/backend/urls.py
+++ back/backend/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from inventory.urls import urlpatterns as inventory_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("inventory/", include(inventory_urlpatterns)),
+    path("", include("payment.urls")),
 ]
