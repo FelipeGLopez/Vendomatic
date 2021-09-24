eb6d3d8758ccf5864f016333adc46e29d63b0135
Felipe <fglf18@gmail.com>
2021-09-24 01:43:06 -0300

    Prettify file

--- front/src/components/Beverage/index.js
+++ front/src/components/Beverage/index.js
@@ -5,26 +5,23 @@ const BeverageList = (props) => {
   const { items, setCoins, setText } = props;
 
   const buyItem = (id, name) => {
-    axiosInstance
-      .put(`/inventory/${id}/`)
-      // .then((response) => response)
-      .then(
-        (response) => {
-          setCoins(response.headers["x-coins"]);
-          setText(`You bought ${response.data.quantity} ${name}!`);
-        },
-        (error) => {
-          if (error.response.status === 404) {
-            setText(
-              `You have ${error.response.headers["x-coins"]} coins!\nThere are no ${name} beverages left!`
-            );
-          } else if (error.response.status === 400) {
-            setText(`You have ${error.response.headers["x-coins"]} coins!`);
-          } else {
-            console.log(error);
-          }
+    axiosInstance.put(`/inventory/${id}/`).then(
+      (response) => {
+        setCoins(response.headers["x-coins"]);
+        setText(`You bought ${response.data.quantity} ${name}!`);
+      },
+      (error) => {
+        if (error.response.status === 404) {
+          setText(
+            `You have ${error.response.headers["x-coins"]} coins!\nThere are no ${name} beverages left!`
+          );
+        } else if (error.response.status === 400) {
+          setText(`You have ${error.response.headers["x-coins"]} coins!`);
+        } else {
+          console.log(error);
         }
-      );
+      }
+    );
   };
 
   return (
